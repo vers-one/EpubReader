cf5caa610f5eab47d925bd2958b52c7263e00595
versfx <versfx@users.noreply.github.com>
2015-05-02 20:23:43 +0300

    Demo app: displaying generic book cover image if no one exists in the book

--- Source/EpubReaderDemo/Entities/Book.cs
+++ Source/EpubReaderDemo/Entities/Book.cs
@@ -11,5 +11,6 @@ namespace EpubReaderDemo.Entities
         public int Id { get; set; }
         public string FilePath { get; set; }
         public string Title { get; set; }
+        public bool HasCover { get; set; }
     }
 }
--- Source/EpubReaderDemo/Entities/Constants.cs
+++ Source/EpubReaderDemo/Entities/Constants.cs
@@ -9,6 +9,7 @@ namespace EpubReaderDemo.Entities
     public class Constants
     {
         public const string COVER_IMAGES_FOLDER = "Covers";
+        public const string GENERIC_COVER_IMAGE_SOURCE = "/Resources/Book.png";
         public const int COVER_IMAGE_MAX_WIDTH = 256;
         public const int COVER_IMAGE_MAX_HEIGHT = 256;
     }
