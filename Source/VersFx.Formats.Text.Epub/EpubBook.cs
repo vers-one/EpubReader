5dce9c95fcaae917b7863d68b60e0a502ea82edb
versfx <versfx@users.noreply.github.com>
2015-05-09 23:15:19 +0300

    EpubBook Authors property value bug fix

--- Source/VersFx.Formats.Text.Epub/EpubBook.cs
+++ Source/VersFx.Formats.Text.Epub/EpubBook.cs
@@ -8,7 +8,8 @@ namespace VersFx.Formats.Text.Epub
     {
         public string FilePath { get; set; }
         public string Title { get; set; }
-        public string Authors { get; set; }
+        public string Author { get; set; }
+        public List<string> AuthorList { get; set; }
         public EpubSchema Schema { get; set; }
         public EpubContent Content { get; set; }
         public Image CoverImage { get; set; }
