0dbabdc10dfbb07ef388ebdabd596e9fa4b6964e
vers-one <12114169+vers-one@users.noreply.github.com>
2022-08-30 21:30:28 -0400

    UrlParser unit tests

---
+++ Source/VersOne.Epub.Test/Unit/Utils/UrlParserTests.cs
@@ -0,0 +1,18 @@
+﻿using VersOne.Epub.Internal;
+
+namespace VersOne.Epub.Test.Unit.Utils
+{
+    public class UrlParserTests
+    {
+        [Theory(DisplayName = "Parsing URLs with or without anchor should succeed")]
+        [InlineData("file.html#anchor", "file.html", "anchor")]
+        [InlineData("file.html", "file.html", null)]
+        [InlineData(null, null, null)]
+        public void ConstructorTest(string url, string expectedPath, string expectedAnchor)
+        {
+            UrlParser urlParser = new(url);
+            Assert.Equal(expectedPath, urlParser.Path);
+            Assert.Equal(expectedAnchor, urlParser.Anchor);
+        }
+    }
+}
