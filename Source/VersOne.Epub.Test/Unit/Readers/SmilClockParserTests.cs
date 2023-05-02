01a780c2c763d070e7fff6032134a352ccc642ac
vers-one <12114169+vers-one@users.noreply.github.com>
2023-05-02 15:39:59 -0400

    SMIL clock parser

---
+++ Source/VersOne.Epub.Test/Unit/Readers/SmilClockParserTests.cs
@@ -0,0 +1,123 @@
+﻿using VersOne.Epub.Readers;
+using VersOne.Epub.Test.Comparers;
+
+namespace VersOne.Epub.Test.Unit.Readers
+{
+    public class SmilClockParserTests
+    {
+        [Fact(DisplayName = "Invalid SMIL clock values parsing test")]
+        public void InvalidClockValuesTest()
+        {
+            TestInvalidValue(null!);
+            TestInvalidValue("");
+            TestInvalidValue(" ");
+        }
+
+        [Fact(DisplayName = "Valid SMIL full clock values parsing test")]
+        public void ValidFullClockValuesTest()
+        {
+            TestValidValue("02:30:03", new EpubNarrationTimestamp(2, 30, 3, 0));
+            TestValidValue("50:00:10.25", new EpubNarrationTimestamp(50, 0, 10, 250));
+            TestValidValue("01:02:03.999", new EpubNarrationTimestamp(1, 2, 3, 999));
+            TestValidValue("01:02:03.9991", new EpubNarrationTimestamp(1, 2, 3, 999));
+            TestValidValue(" 1:02:03.4", new EpubNarrationTimestamp(1, 2, 3, 400));
+            TestValidValue("1:02:03.4 ", new EpubNarrationTimestamp(1, 2, 3, 400));
+            TestValidValue(" 1:02:03.4 ", new EpubNarrationTimestamp(1, 2, 3, 400));
+        }
+
+        [Fact(DisplayName = "Invalid SMIL full clock values parsing test")]
+        public void InvalidFullClockValuesTest()
+        {
+            TestInvalidValue("1:02:03:04");
+            TestInvalidValue("1:02:03:04.5");
+            TestInvalidValue(":02:03");
+            TestInvalidValue("1:02:");
+            TestInvalidValue("a:02:03");
+            TestInvalidValue("-1:02:03");
+            TestInvalidValue("1:60:03");
+            TestInvalidValue("1:a:03");
+            TestInvalidValue("1:-1:03");
+            TestInvalidValue("1:02:60");
+            TestInvalidValue("1:02:a");
+            TestInvalidValue("1:02:-1");
+            TestInvalidValue("1:02:03.a");
+            TestInvalidValue("1:02:03.-1");
+            TestInvalidValue("1:02:03,1");
+        }
+
+        [Fact(DisplayName = "Valid SMIL partial clock values parsing test")]
+        public void ValidPartialClockValuesTest()
+        {
+            TestValidValue("02:33", new EpubNarrationTimestamp(0, 2, 33, 0));
+            TestValidValue("00:10.5", new EpubNarrationTimestamp(0, 0, 10, 500));
+            TestValidValue("1:02.999", new EpubNarrationTimestamp(0, 1, 2, 999));
+            TestValidValue("1:02.9991", new EpubNarrationTimestamp(0, 1, 2, 999));
+            TestValidValue(" 1:02.3", new EpubNarrationTimestamp(0, 1, 2, 300));
+            TestValidValue("1:02.3 ", new EpubNarrationTimestamp(0, 1, 2, 300));
+            TestValidValue(" 1:02.3 ", new EpubNarrationTimestamp(0, 1, 2, 300));
+        }
+
+        [Fact(DisplayName = "Invalid SMIL partial clock values parsing test")]
+        public void InvalidPartialClockValuesTest()
+        {
+            TestInvalidValue("1:");
+            TestInvalidValue(":01");
+            TestInvalidValue("60:02");
+            TestInvalidValue("a:02");
+            TestInvalidValue("-1:02");
+            TestInvalidValue("1:60");
+            TestInvalidValue("1:a");
+            TestInvalidValue("1:-1");
+            TestInvalidValue("1:02.a");
+            TestInvalidValue("1:02.-1");
+            TestInvalidValue("1:02,1");
+        }
+
+        [Fact(DisplayName = "Valid SMIL timecount values parsing test")]
+        public void ValidTimecountValuesTest()
+        {
+            TestValidValue("3h", new EpubNarrationTimestamp(3, 0, 0, 0));
+            TestValidValue("45min", new EpubNarrationTimestamp(0, 45, 0, 0));
+            TestValidValue("30s", new EpubNarrationTimestamp(0, 0, 30, 0));
+            TestValidValue("5ms", new EpubNarrationTimestamp(0, 0, 0, 5));
+            TestValidValue("12", new EpubNarrationTimestamp(0, 0, 12, 0));
+            TestValidValue("3.2h", new EpubNarrationTimestamp(3, 12, 0, 0));
+            TestValidValue("45.5min", new EpubNarrationTimestamp(0, 45, 30, 0));
+            TestValidValue("30.1s", new EpubNarrationTimestamp(0, 0, 30, 100));
+            TestValidValue("12.467", new EpubNarrationTimestamp(0, 0, 12, 467));
+            TestValidValue("1.999", new EpubNarrationTimestamp(0, 0, 1, 999));
+            TestValidValue("1.9991", new EpubNarrationTimestamp(0, 0, 1, 999));
+            TestValidValue("300h", new EpubNarrationTimestamp(300, 0, 0, 0));
+            TestValidValue("3.3333333h", new EpubNarrationTimestamp(3, 19, 59, 999));
+            TestValidValue("130.151234min", new EpubNarrationTimestamp(2, 10, 9, 74));
+            TestValidValue("3725.1234s", new EpubNarrationTimestamp(1, 2, 5, 123));
+            TestValidValue("11531567.8ms", new EpubNarrationTimestamp(3, 12, 11, 567));
+            TestValidValue(" 10ms", new EpubNarrationTimestamp(0, 0, 0, 10));
+            TestValidValue("10ms ", new EpubNarrationTimestamp(0, 0, 0, 10));
+            TestValidValue(" 10ms ", new EpubNarrationTimestamp(0, 0, 0, 10));
+        }
+
+        [Fact(DisplayName = "Invalid SMIL timecount values parsing test")]
+        public void InvalidTimecountValuesTest()
+        {
+            TestInvalidValue("1m");
+            TestInvalidValue("1H");
+            TestInvalidValue("1 h");
+            TestInvalidValue("ah");
+            TestInvalidValue("-1h");
+            TestInvalidValue("-1.5h");
+            TestInvalidValue($"{(decimal)Int32.MaxValue * 60 * 60 * 1000 + 1}ms");
+        }
+
+        private static void TestValidValue(string value, EpubNarrationTimestamp expectedResult)
+        {
+            Assert.True(SmilClockParser.TryParse(value, out EpubNarrationTimestamp actualResult));
+            EpubNarrationComparers.CompareEpubNarrationTimestamps(expectedResult, actualResult);
+        }
+
+        private static void TestInvalidValue(string value)
+        {
+            Assert.False(SmilClockParser.TryParse(value, out _));
+        }
+    }
+}
---
+++ Source/VersOne.Epub/Readers/SmilClockParser.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Globalization;
+
+namespace VersOne.Epub.Readers
+{
+    internal static class SmilClockParser
+    {
+        private const int MILLISECONDS_IN_SECOND = 1000;
+        private const int MILLISECONDS_IN_MINUTE = 60 * MILLISECONDS_IN_SECOND;
+        private const int MILLISECONDS_IN_HOUR = 60 * MILLISECONDS_IN_MINUTE;
+        private const decimal MAX_TIMECOUNT = (decimal)Int32.MaxValue * MILLISECONDS_IN_HOUR;
+
+        public static bool TryParse(string smilClock, out EpubNarrationTimestamp result)
+        {
+            if (string.IsNullOrWhiteSpace(smilClock))
+            {
+                result = default;
+                return false;
+            }
+            smilClock = smilClock.Trim();
+            if (smilClock.Contains(":"))
+            {
+                return TryParseClockValue(smilClock, out result);
+            }
+            else
+            {
+                return TryParseTimecountValue(smilClock, out result);
+            }
+        }
+
+        private static bool TryParseClockValue(string clockValue, out EpubNarrationTimestamp result)
+        {
+            string[] parts = clockValue.Split(':');
+            if (parts.Length > 3)
+            {
+                result = default;
+                return false;
+            }
+            int hour;
+            int currentPartIndex;
+            if (parts.Length == 3)
+            {
+                if (!Int32.TryParse(parts[0], out hour) || hour < 0)
+                {
+                    result = default;
+                    return false;
+                }
+                currentPartIndex = 1;
+            }
+            else
+            {
+                hour = 0;
+                currentPartIndex = 0;
+            }
+            if (!Int32.TryParse(parts[currentPartIndex], out int minute) || minute < 0 || minute >= 60)
+            {
+                result = default;
+                return false;
+            }
+            currentPartIndex++;
+            if (!Decimal.TryParse(parts[currentPartIndex], NumberStyles.AllowDecimalPoint, CultureInfo.InvariantCulture, out decimal secondWithFraction) ||
+                secondWithFraction < 0 || secondWithFraction >= 60)
+            {
+                result = default;
+                return false;
+            }
+            int second = (int)Math.Truncate(secondWithFraction);
+            int millisecond = (int)Math.Truncate((secondWithFraction - second) * 1000);
+            result = new EpubNarrationTimestamp(hour, minute, second, millisecond);
+            return true;
+        }
+
+        private static bool TryParseTimecountValue(string timecountValue, out EpubNarrationTimestamp result)
+        {
+            bool hasMetric = false;
+            int metricStartIndex = -1;
+            for (int i = 0; i < timecountValue.Length; i++)
+            {
+                char c = timecountValue[i];
+                if (!Char.IsDigit(c) && c != '.')
+                {
+                    hasMetric = true;
+                    metricStartIndex = i;
+                    break;
+                }
+            }
+            string timecountWithFractionString = hasMetric ? timecountValue.Substring(0, metricStartIndex) : timecountValue;
+            if (!Decimal.TryParse(timecountWithFractionString, NumberStyles.AllowDecimalPoint, CultureInfo.InvariantCulture, out decimal timecountWithFraction) ||
+                timecountWithFraction > MAX_TIMECOUNT)
+            {
+                result = default;
+                return false;
+            }
+            int metricMultiplier;
+            if (hasMetric)
+            {
+                string metric = timecountValue.Substring(metricStartIndex);
+                switch (metric)
+                {
+                    case "h":
+                        metricMultiplier = MILLISECONDS_IN_HOUR;
+                        break;
+                    case "min":
+                        metricMultiplier = MILLISECONDS_IN_MINUTE;
+                        break;
+                    case "s":
+                        metricMultiplier = MILLISECONDS_IN_SECOND;
+                        break;
+                    case "ms":
+                        metricMultiplier = 1;
+                        break;
+                    default:
+                        result = default;
+                        return false;
+                }
+            }
+            else
+            {
+                metricMultiplier = MILLISECONDS_IN_SECOND;
+            }
+            decimal milliseconds = Math.Truncate(timecountWithFraction * metricMultiplier);
+            result = ConvertMillisecondsToEpubNarrationTimestamp(milliseconds);
+            return true;
+        }
+
+        private static EpubNarrationTimestamp ConvertMillisecondsToEpubNarrationTimestamp(decimal milliseconds)
+        {
+            int millisecond = (int)(milliseconds % MILLISECONDS_IN_SECOND);
+            int second = (int)(Math.Truncate(milliseconds / MILLISECONDS_IN_SECOND) % 60);
+            int minute = (int)(Math.Truncate(milliseconds / MILLISECONDS_IN_MINUTE) % 60);
+            int hour = (int)Math.Truncate(milliseconds / MILLISECONDS_IN_HOUR);
+            return new(hour, minute, second, millisecond);
+        }
+    }
+}
