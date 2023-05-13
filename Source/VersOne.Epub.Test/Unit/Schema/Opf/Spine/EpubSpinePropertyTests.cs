aceec15d6f50ead4257ba5293d063e46a88421db
vers-one <12114169+vers-one@users.noreply.github.com>
2023-05-13 00:39:37 -0400

    New EPUB spine properties

--- Source/VersOne.Epub.Test/Unit/Schema/Opf/Spine/EpubSpinePropertyTests.cs
+++ Source/VersOne.Epub.Test/Unit/Schema/Opf/Spine/EpubSpinePropertyTests.cs
@@ -7,11 +7,34 @@ namespace VersOne.Epub.Test.Unit.Schema.Opf.Spine
         [Fact(DisplayName = "Converting a string to EpubSpineProperty list should succeed")]
         public void ParsePropertyListTest()
         {
-            string stringValue = "page-spread-left page-spread-right test-unknown-property";
+            string stringValue = "rendition:layout-pre-paginated rendition:layout-reflowable " +
+                "rendition:orientation-auto rendition:orientation-landscape rendition:orientation-portrait " +
+                "rendition:spread-auto rendition:spread-both rendition:spread-landscape rendition:spread-none " +
+                "rendition:page-spread-center rendition:page-spread-left page-spread-left rendition:page-spread-right page-spread-right " +
+                "rendition:flow-paginated rendition:flow-scrolled-continuous rendition:flow-scrolled-doc rendition:flow-auto " +
+                "rendition:align-x-center " +
+                "test-unknown-property";
             List<EpubSpineProperty> expectedPropertyList = new()
             {
+                EpubSpineProperty.LAYOUT_PRE_PAGINATED,
+                EpubSpineProperty.LAYOUT_REFLOWABLE,
+                EpubSpineProperty.ORIENTATION_AUTO,
+                EpubSpineProperty.ORIENTATION_LANDSCAPE,
+                EpubSpineProperty.ORIENTATION_PORTRAIT,
+                EpubSpineProperty.SPREAD_AUTO,
+                EpubSpineProperty.SPREAD_BOTH,
+                EpubSpineProperty.SPREAD_LANDSCAPE,
+                EpubSpineProperty.SPREAD_NONE,
+                EpubSpineProperty.PAGE_SPREAD_CENTER,
+                EpubSpineProperty.PAGE_SPREAD_LEFT,
                 EpubSpineProperty.PAGE_SPREAD_LEFT,
                 EpubSpineProperty.PAGE_SPREAD_RIGHT,
+                EpubSpineProperty.PAGE_SPREAD_RIGHT,
+                EpubSpineProperty.FLOW_PAGINATED,
+                EpubSpineProperty.FLOW_SCROLLED_CONTINUOUS,
+                EpubSpineProperty.FLOW_SCROLLED_DOC,
+                EpubSpineProperty.FLOW_AUTO,
+                EpubSpineProperty.ALIGN_X_CENTER,
                 EpubSpineProperty.UNKNOWN
             };
             List<EpubSpineProperty> actualPropertyList = EpubSpinePropertyParser.ParsePropertyList(stringValue);
